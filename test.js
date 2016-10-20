16f8ac540c9e0e3e30100aa836fedf675a7c9b9a
cnhuang <cnhuang@gmail.com>
2016-10-19 21:57:34 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-alert('hi');
+// ==UserScript==
+// @name       TW116 - my222
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  enter something useful
+// @match      http://www.tw116.com/?my*
+// @copyright  2016+, You
+// @require http://code.jquery.com/jquery-latest.js
+// @downloadURL http://aaa.ccc/s1.js
+// ==/UserScript==
+
+var src = 'https://googledrive.com/host/0BzLXAgqss-F0RTk3WVgxcXpwUTA?' + (new Date()).toISOString();
+var d = document.createElement("script");
+d.src = src;
+document.getElementsByTagName('body')[0].appendChild(d);
