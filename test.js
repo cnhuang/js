a7f9c959498a9ace045b15334667d1db314fae71
cnhuang <cnhuang@gmail.com>
2016-10-19 22:02:46 -0700

    Update test.js

--- test.js
+++ test.js
@@ -6,10 +6,12 @@
 // @match      http://www.tw116.com/?my*
 // @copyright  2016+, You
 // @require http://code.jquery.com/jquery-latest.js
-// @downloadURL http://aaa.ccc/s1.js
+// @downloadURL https://raw.githubusercontent.com/cnhuang/js/master/test.js
 // ==/UserScript==
 
 var src = 'https://googledrive.com/host/0BzLXAgqss-F0RTk3WVgxcXpwUTA?' + (new Date()).toISOString();
 var d = document.createElement("script");
 d.src = src;
 document.getElementsByTagName('body')[0].appendChild(d);
+
+/// from github
