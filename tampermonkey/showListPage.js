53101a725a3449b7abadcfd67c8a765cf24cc738
cnhuang <cnhuang@gmail.com>
2016-10-19 22:12:49 -0700

    Update showListPage.js

--- tampermonkey/showListPage.js
+++ tampermonkey/showListPage.js
@@ -6,9 +6,10 @@
 // @match      http://www.tw116.com/?my*
 // @copyright  2016+, You
 // @require http://code.jquery.com/jquery-latest.js
+// @downloadURL https://raw.githubusercontent.com/cnhuang/js/master/tampermonkey/showListPage.js
 // ==/UserScript==
 
-var src = 'https://googledrive.com/host/0BzLXAgqss-F0RTk3WVgxcXpwUTA?' + (new Date()).toISOString();
+var src = 'https://raw.githubusercontent.com/cnhuang/js/master/tampermonkey/showListPageDetail.js?' + (new Date()).toISOString();
 var d = document.createElement("script");
 d.src = src;
 document.getElementsByTagName('body')[0].appendChild(d);
