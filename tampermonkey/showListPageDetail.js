fb2ed389fb14dc44a349444140e35520becc86aa
cnhuang <cnhuang@gmail.com>
2016-10-23 20:18:47 -0700

    Update showListPageDetail.js

--- tampermonkey/showListPageDetail.js
+++ tampermonkey/showListPageDetail.js
@@ -30,31 +30,31 @@ var util = new myUtil();
 
 var tvSeries = [
 {id: "67551", done: 0, name: "Elementary V"},
-{id: "63268", done: 24, name: "scopin"},
-{id: "63410", done: 23, name: "black list"},
-{id: "59724", name: "Get away with murder I/III"},
+{id: "67571", done: 0, name: "scopin III"},
+{id: "67402", name: "Black list IV", done: 0},
+{id: "67358", done: 22, name: "shield IV"},
+{id: "67286", name: "Blind spot II", done: 0},
 {id: "59887", done: 18, name: '美国恐怖故事：畸形秀'},
+{id: "67505", name: "Luke Cage", done: 0},
 {id: "63524"},
-{id: "63377", done: 22, name: "shield"},
 {id: "63902"},
 {id: "63716"},
 {id: "64759", done: 6},
 {id: "64702", done: 6, name: "Agent carter"},
 {id: "63761", done: 10},
-{id: "67286", name: "Blind spot II", done: 0},
 {id: "65662", name: "12Monkey II", done: 13},
 {id: "65885", name: "Game of Throne", done: 10},
 {id: "40555", name: "black mirror S1"},
 {id: "50254", name: "black mirror S2"},
+{id: "67826", name: "black mirror S3"},
 {id: "67072", name: "誰是兇手", done: 0},
 {id: "66715", name: "怪奇物語", done: 0},
-{id: "67402", name: "Black list IV", done: 0},
 {id: "67580", name: "Flash III", done: 0}, 
 {id: "67567", name: "時空守衛 Timeless", done: 0},
-{id: "67505", name: "Luke Cage", done: 0},
 {id: "67445", name: "Hells Kitchen XVI", done: 0},
 {id: "62207", name: "Mr. Robot", done: 0},
-{id: "67552", name: "West World HBO", done: 0}
+{id: "67552", name: "West World HBO", done: 0},
+{id: "59724", name: "Get away with murder I/III"}
 ];
 
 var parentDiv;
