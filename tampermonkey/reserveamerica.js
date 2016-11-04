6c9829c7492840cc506fbb011b7a062c4987cddc
cnhuang <cnhuang@gmail.com>
2016-11-03 22:19:05 -0700

    Update reserveamerica.js

--- tampermonkey/reserveamerica.js
+++ tampermonkey/reserveamerica.js
@@ -10,7 +10,7 @@
             $('#passwrdGroup input')[0].value = 'g3sj61u3';
             $('#submitForm_submitForm').click();
         } else if ($('#Search')) {
-            window.location.href = 'http://www.reserveamerica.com/switchBookingAction.do?contractCode=CA&parkId=120063&siteId=390&camparea=52762255&selStatus=null&matrixHasError=true&dateToday=03%2F30%2F2017%C2%A4tMaximumWindow=12&dateMinWindow=11%2F04%2F2016&dateMaxWindow=11%2F02%2F2017&arvdate=03%2F30%2F2017&arrivaldate=Thu+Mar+30+2017&lengthOfStay=1&dateChosen=true';
+            window.location.href = 'https://www.reserveamerica.com/switchBookingAction.do?contractCode=CA&parkId=120063&siteId=390&camparea=52762255&selStatus=null&matrixHasError=true&dateToday=03%2F30%2F2017%C2%A4tMaximumWindow=12&dateMinWindow=11%2F04%2F2016&dateMaxWindow=11%2F02%2F2017&arvdate=03%2F30%2F2017&arrivaldate=Thu+Mar+30+2017&lengthOfStay=1&dateChosen=true';
         }
     };
     
