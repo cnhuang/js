c0e91a16c28fb829e2a196f006fcf6c3c8d8b20a
cnhuang <cnhuang@gmail.com>
2016-11-03 22:22:06 -0700

    Update reserveamerica.js

--- tampermonkey/reserveamerica.js
+++ tampermonkey/reserveamerica.js
@@ -6,8 +6,10 @@
     
     let login = () => {
         if ($('#Search')) {
+            console.log('Logged in');
             window.location.href = 'https://www.reserveamerica.com/switchBookingAction.do?contractCode=CA&parkId=120063&siteId=390&camparea=52762255&selStatus=null&matrixHasError=true&dateToday=03%2F30%2F2017%C2%A4tMaximumWindow=12&dateMinWindow=11%2F04%2F2016&dateMaxWindow=11%2F02%2F2017&arvdate=03%2F30%2F2017&arrivaldate=Thu+Mar+30+2017&lengthOfStay=1&dateChosen=true';
         } else if ($('progresspopup') && ($('progresspopup').length == 0 || $('progresspopup').style.display == 'none')) {
+            console.log('Start to login...');
             $('#combinedFlowSignInKit_emailGroup_attrs input')[0].value = 'cnhuang@gmail.com';
             $('#passwrdGroup input')[0].value = 'g3sj61u3';
             $('#submitForm_submitForm').click();
@@ -34,10 +36,8 @@
         const url = window.location.href;
         console.log(url);
         if (url.indexOf('memberSignInSignUp.do') >= 0) {
-          console.log('memberSignInSignUp');
           setTimeout(login, 2000);
         } else if (url.indexOf('welcome.do') >= 0) {
-          console.log('welcome');
           goToSignInPage();
         }
     };
