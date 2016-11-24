2363311ab0cc38abf40759610d782307ecbfe2ae
cnhuang <cnhuang@gmail.com>
2016-11-23 22:40:53 -0800

    Update showListPageDetail.js

--- tampermonkey/showListPageDetail.js
+++ tampermonkey/showListPageDetail.js
@@ -46,10 +46,8 @@ Util.getNewElement = (style = {}) => {
 // MyTw116
 class MyTw116 {
 
-  constructor(element, tvData, movieData){
+  constructor(element){
     this.element = element;
-    //this.tvData = tvData;
-    //this.movieData = movieData;    
     this.parentDiv = this.addParentDiv();
     
     this.init();
@@ -64,8 +62,6 @@ class MyTw116 {
       this.headerContainer = Util.appendNewElement(this.parentDiv);
       this.bodyContainer = Util.appendNewElement(this.parentDiv);
 
-      console.log(tvData);
-      console.log(movieData);
       this.loadTvTab(tvData).onclick();
       this.loadMovieTab(movieData);
 
@@ -78,16 +74,11 @@ class MyTw116 {
   
   // Movie Tab
   loadMovieTab(data) {
-    const render = (tv, url, all, unWatched) => {      
-      const tvName = tv.name || (() => {
-        if (all.length == 0)
-          return 'Unknown';
-        else 
-          return /mz=(.*?)S/.exec(all[0].url) || `Can't parse from ${all[0].url}`;
-      })();
+    const render = (show, url, episodes) => {      
+      const showName = this.getShowName(show, episodes);
 
-      let html = `<div><a target=_blank href='${url}'>${tvName}</a></div>`;
-      unWatched.forEach((e) => {
+      let html = `<div><a target=_blank href='${url}'>${showName}</a></div>`;
+      episodes.forEach((e) => {
         html += `<span style="padding-right:20px;" id="${e.id}"><a href="${e.url}">${e.name}</a></span>`;
       });
       return html;
@@ -97,16 +88,11 @@ class MyTw116 {
 
   // TV Tab
   loadTvTab(data) {
-    const render = (tv, url, all, unWatched) => {      
-      const tvName = tv.name || (() => {
-        if (all.length == 0)
-          return 'Unknown';
-        else 
-          return /mz=(.*?)S/.exec(all[0].url) || `Can't parse from ${all[0].url}`;
-      })();
+    const render = (show, url, episodes) => {      
+      const showName = this.getShowName(show, episodes);
 
-      let html = `<div><a target=_blank href='${url}'>${tvName} (${tv.id}, watched: ${tv.done || 0})</a></div>`;
-      unWatched.forEach((e) => {
+      let html = `<div><a target=_blank href='${url}'>${showName} (${show.id}, watched: ${show.done || 0})</a></div>`;
+      episodes.forEach((e) => {
         html += `<span style="padding-right:20px;" id="${e.id}"><a href="${e.url}">${e.name}</a></span>`;
       });
       return html;
@@ -125,38 +111,37 @@ class MyTw116 {
   }
   
   // Utility
-  addTab(data, render, id, innerText) {
+  addTab(shows, render, id, innerText) {
     let tab = Util.appendNewElement(this.headerContainer, this.tabStyle());
     tab.innerText = innerText;
     
     const content = Util.appendNewElement(this.bodyContainer, {display: 'none'});
-    this.loadData(content, this.parseData, data, render);
+    this.loadData(content, this.parseData, shows, render);
     this.tabs[id] = content;
     tab.onclick = () => this.openTab(id);        
     return tab;
   }
     
-  loadData(element, parseFunc, data, render) {
-    const successCallback = (d, url, content) => {
-      parseFunc(element, d, url, content, render);
+  loadData(element, parseFunc, shows, render) {
+    const successCallback = (show, url, content) => {
+      parseFunc(element, show, url, content, render);
     };
 
-    const failCallabck = (d, url) => {
-      console.log(`Error: ${url} - ${d}`);
+    const failCallabck = (show, url) => {
+      console.log(`Error: ${url} - ${show}`);
     };
 
-    data.forEach((d, index) =>{
-      const url = `http://www.tw116.com/vod-play-id-${d.id}-sid-0-pid-0.html`;
-      $.get(url, successCallback.bind(this, d, url))
-        .fail(failCallabck(d, url));
+    shows.forEach((show, index) =>{
+      const url = `http://www.tw116.com/vod-play-id-${show.id}-sid-0-pid-0.html`;
+      $.get(url, successCallback.bind(this, show, url))
+        .fail(failCallabck(show, url));
     });
   }
   
-  parseData(parentDiv, data, url, content, render) {  
+  parseData(parentDiv, show, url, content, render) {  
     const url_list = content.match(/var\s+url_list.*?;/);
     const players = decodeURIComponent(url_list).split('$$$');
-    const unWatched = [];
-    const all = [];
+    const episodes = [];
 
     if (players) {
       const player = players.find((p) => p.includes('xfplay://')) || '';
@@ -165,17 +150,14 @@ class MyTw116 {
         const tokens = link.split('++');
         const name = tokens[0];
         const url = tokens[1];
-        const id = `${data.id}-${index}`;
-        
-        if (!data.done || data.done <= index) {
-          unWatched.push({name, url, id});
-        }
-        all.push({name, url, id});
+        const id = `${show.id}-${index}`;
+        const watched = show.done && show.done > index;
+        episodes.push({name, url, id, watched});
       });
     }
 
     var d = Util.appendNewElement(parentDiv, {marginBottom: '20px'});
-    d.innerHTML = render(data, url, all, unWatched);
+    d.innerHTML = render(show, url, episodes);
   }
   
   openTab(id) {
@@ -214,40 +196,16 @@ class MyTw116 {
       display: 'inline-block'
     };
   }
+  
+  getShowName(show, episodes) {
+    return show.name || (() => {
+      if (episodes.length == 0)
+        return 'Unknown';
+      else 
+        return /mz=(.*?)S/.exec(episodes[0].url) || `Can't parse from ${episodes[0].url}`;
+    })();
+  }
 };
 
-// Data
-const tvSeries2 = [
-  {id: "67551", done: 1, name: "Elementary V"},
-  {id: "67571", done: 1, name: "scopin III"},
-  {id: "67402", name: "Black list IV", done: 8},
-  {id: "67358", done: 6, name: "shield IV"},
-  {id: "67286", name: "Blind spot II", done: 8},
-  {id: "59887", done: 18, name: '美国恐怖故事：畸形秀'},
-  {id: "67505", name: "Luke Cage", done: 1},
-  {id: "67567", name: "時空守衛 Timeless", done: 5},
-  {id: "63524", name: "美国恐怖故事第五季"},
-  {id: "63902"},
-  {id: "63716"},
-  {id: "64759", done: 6},
-  {id: "64702", done: 6, name: "Agent carter"},
-  {id: "63761", done: 10},
-  {id: "65662", name: "12Monkey II", done: 13},
-  {id: "65885", name: "Game of Throne", done: 10},
-  {id: "40555", name: "black mirror S1"},
-  {id: "50254", name: "black mirror S2"},
-  {id: "67826", name: "black mirror S3"},
-  {id: "67072", name: "誰是兇手", done: 0},
-  {id: "66715", name: "怪奇物語", done: 0},
-  {id: "67580", name: "Flash III", done: 0}, 
-  {id: "67445", name: "Hells Kitchen XVI", done: 0},
-  {id: "62207", name: "Mr. Robot", done: 0},
-  {id: "67552", name: "West World HBO", done: 0},
-  {id: "59724", name: "Get away with murder I/III"}
-];
-
-const movies2 = [
-  {id: "66111"}
-];
 // Main function
-Util.loadJQuery(() => new MyTw116(document.getElementsByTagName('body')[0], tvSeries2, movies2));
+Util.loadJQuery(() => new MyTw116(document.getElementsByTagName('body')[0]));
