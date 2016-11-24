046ac99179cf5e8ca175037af3f3c659b2790f1d
cnhuang <cnhuang@gmail.com>
2016-11-23 21:49:15 -0800

    Create shows.js

---
+++ tampermonkey/shows.js
@@ -0,0 +1,34 @@
+
+// Data
+const tvSeries = [
+  {id: "67551", done: 1, name: "Elementary V"},
+  {id: "67571", done: 1, name: "scopin III"},
+  {id: "67402", name: "Black list IV", done: 8},
+  {id: "67358", done: 6, name: "shield IV"},
+  {id: "67286", name: "Blind spot II", done: 8},
+  {id: "59887", done: 18, name: '美国恐怖故事：畸形秀'},
+  {id: "67505", name: "Luke Cage", done: 1},
+  {id: "67567", name: "時空守衛 Timeless", done: 5},
+  {id: "63524", name: "美国恐怖故事第五季"},
+  {id: "63902"},
+  {id: "63716"},
+  {id: "64759", done: 6},
+  {id: "64702", done: 6, name: "Agent carter"},
+  {id: "63761", done: 10},
+  {id: "65662", name: "12Monkey II", done: 13},
+  {id: "65885", name: "Game of Throne", done: 10},
+  {id: "40555", name: "black mirror S1"},
+  {id: "50254", name: "black mirror S2"},
+  {id: "67826", name: "black mirror S3"},
+  {id: "67072", name: "誰是兇手", done: 0},
+  {id: "66715", name: "怪奇物語", done: 0},
+  {id: "67580", name: "Flash III", done: 0}, 
+  {id: "67445", name: "Hells Kitchen XVI", done: 0},
+  {id: "62207", name: "Mr. Robot", done: 0},
+  {id: "67552", name: "West World HBO", done: 0},
+  {id: "59724", name: "Get away with murder I/III"}
+];
+
+const movies = [
+  {id: "66111"}
+];
