8b255b29b8b944b191a67ae8836b57c5ed67fc4a
cnhuang <cnhuang@gmail.com>
2017-01-04 22:07:44 -0800

    Update shows.js

--- tampermonkey/shows.js
+++ tampermonkey/shows.js
@@ -3,7 +3,7 @@ const SHOW = (url, done = 0, name = undefined) => ({url, name, done});
 
 // Data
 tvData = [
-  SHOW('http://www.tw116.com/occident/fuermosijibenyanyifadiwuji/', 4, 'Elementary V'),
+  SHOW('http://www.tw116.com/occident/fuermosijibenyanyifadiwuji/', 8, 'Elementary V'),
   SHOW('http://www.tw116.com/occident/zuieheimingdandisiji/', 8, '罪恶黑名单'),
   SHOW('http://www.tw116.com/occident/tianxiedisanji/', 10, 'scopin III'), //# -1. e.g. 10 will block to 11
   SHOW('http://www.tw116.com/occident/shendunjutegongdisiji/', 8, 'shield IV'),
