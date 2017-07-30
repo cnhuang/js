8dbbcc32ca87d3b5f307bc72e0e1c768e2bcbd43
cnhuang <cnhuang@gmail.com>
2017-07-30 10:37:48 -0700

    Update shows.js

--- tampermonkey/shows.js
+++ tampermonkey/shows.js
@@ -34,7 +34,8 @@ tvData = [
   SHOW('http://www.tw116.com/occident/shisangeyuanyindiyiji/', 13, '13 reasons'),
   SHOW('http://www.tw116.com/occident/daxiaohuangyanxiaohuangdashidiyiji/', 3, ''),
   SHOW('http://www.tw116.com/occident/miwudiyiji/', 0, ''),
-  SHOW('http://www.tw116.com/occident/heianwuzhidiyiji/', 0, 'Dark matters'),
+  SHOW('http://www.tw116.com/occident/heianwuzhidierji/', 0, 'Dark matters2'),
+  SHOW('http://www.tw116.com/occident/heianwuzhidisanji/', 0, 'Dark matters3'),
 ];
 
 movieData = [
