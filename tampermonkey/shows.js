fbf7de56213165d5d84eba30370f13074d90e8c7
cnhuang <cnhuang@gmail.com>
2017-10-22 18:35:38 -0700

    Update shows.js

--- tampermonkey/shows.js
+++ tampermonkey/shows.js
@@ -184,4 +184,5 @@ cartoonData = [
   SHOW('http://www.tw116.com/anime/meishizongdongyuanliaolishuwang/'),
   SHOW('http://www.tw116.com/anime/longmiaogebideteteluo/'),
   SHOW('http://www.tw116.com/comedy/padingdunxiong/'),
+  SHOW('http://www.tw116.com/anime/zhongfanzhuluoji/'),
 ];
