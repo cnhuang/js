16330a7c46dd839c35e26b75a240cabe91dfbbaa
cnhuang <cnhuang@gmail.com>
2018-01-05 23:47:56 -0800

    Update tw116PlayScript_install.js

--- tampermonkey/tw116PlayScript_install.js
+++ tampermonkey/tw116PlayScript_install.js
@@ -3,7 +3,7 @@
 // @namespace  http://use.i.E.your.homepage/
 // @version    0.1
 // @description  enter something useful
-// @match      http://www.tw116.com/*/*/
+// @match      http://tw116.com/*/*/
 // @copyright  2016+, You
 // @require http://code.jquery.com/jquery-latest.js
 // @downloadURL https://raw.githubusercontent.com/cnhuang/js/master/tampermonkey/tw116PlayScript.js
