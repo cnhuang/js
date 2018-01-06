8dfd1f3f34fec00efd45e87c7e6164660e8e91be
cnhuang <cnhuang@gmail.com>
2018-01-05 23:41:53 -0800

    Create tw116PlayScript_install.js

---
+++ tampermonkey/tw116PlayScript_install.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name       TW116 - showListPage
+// @namespace  http://use.i.E.your.homepage/
+// @version    0.1
+// @description  enter something useful
+// @match      http://www.tw116.com/?my*
+// @copyright  2016+, You
+// @require http://code.jquery.com/jquery-latest.js
+// @downloadURL https://raw.githubusercontent.com/cnhuang/js/master/tampermonkey/tw116PlayScript.js
+// ==/UserScript==
+
+var src = 'https://raw.githubusercontent.com/cnhuang/js/master/tampermonkey/tw116PlayScript.js?' + (new Date()).toISOString();
+var stringData = $.ajax({
+                    url: src,
+                    async: false
+                 }).responseText;
+eval(stringData);
