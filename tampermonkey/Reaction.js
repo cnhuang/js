da02c1b9144079987873aa3d810669646a5c3721
cnhuang <cnhuang@gmail.com>
2020-01-12 14:02:48 -0800

    Update Reaction.js

--- tampermonkey/Reaction.js
+++ tampermonkey/Reaction.js
@@ -1,18 +1,21 @@
+(function() {
+    'use strict';
 
-function getElm(selector) {
-  return $(selector).length == 0 ? null : $(selector)[0];
-};
+    function getElm(selector) {
+      return $(selector).length == 0 ? null : $(selector)[0];
+    };
 
-function addControlPanel() {
-  const header = getElm('header.nav-header-container');
-  let panel = getElm('#control-panel');
-  if (!panel) {
-    panel = $(`<div id='control-panel' style='width:100%;height:100px;'>
-                    <input plcaseholder="StartDate yyyy-mm-dd">
-                  </div>`);
-    header.append(panel);
-    console.log('added control panel)';
-  }
-}
+    function addControlPanel() {
+      const header = getElm('header.nav-header-container');
+      let panel = getElm('#control-panel');
+      if (!panel) {
+        panel = $(`<div id='control-panel' style='width:100%;height:100px;'>
+                        <input plcaseholder="StartDate yyyy-mm-dd">
+                      </div>`);
+        header.append(panel);
+        console.log('added control panel)';
+      }
+    }
 
-addControlPanel();
+    addControlPanel();
+})();
