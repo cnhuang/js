4ef691af4a2d10ccc8153995b9d4e63cc802b70e
cnhuang <cnhuang@gmail.com>
2020-01-12 13:52:15 -0800

    Update Reaction.js

--- tampermonkey/Reaction.js
+++ tampermonkey/Reaction.js
@@ -1,1 +1,18 @@
 
+function getElm(selector) {
+  return $(selector).length == 0 ? null : $(selector)[0];
+};
+
+function addControlPanel() {
+  const header = getElm('header.nav-header-container');
+  let panel = getElm('#control-panel');
+  if (!panel) {
+    panel = $(`<div id='control-panel' style='width:100%;height:100px;'>
+                    <input plcaseholder="StartDate yyyy-mm-dd">
+                  </div>`);
+    header.append(panel);
+    console.log('added control panel)';
+  }
+}
+
+addControlPanel();
