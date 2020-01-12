326052df18477fa3376b56d624adf83511ac4a7c
cnhuang <cnhuang@gmail.com>
2020-01-12 13:42:39 -0800

    Create Reaction_install.js

---
+++ tampermonkey/Reaction_install.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Reaction Install
+// @namespace    http://www.recreation.gov/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://www.recreation.gov*
+// @require      http://code.jquery.com/jquery-latest.js
+// @grant        none
+// ==/UserScript==
+
+let src = 'https://raw.githubusercontent.com/cnhuang/js/master/tampermonkey/Reaction.js?' + (new Date()).toISOString();
+let stringData = $.ajax({
+                    url: src,
+                    async: false
+                 }).responseText;
+eval(stringData);
